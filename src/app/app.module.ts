e1d2c9fcec567c2219afa67e61e908615391b465
sausageRoll <drozdovigor1996@gmail.com>
2020-05-03 15:55:24 +0200

    Add tweet list with approve/decline option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {ClustersComponent} from './clusters/clusters.component';
 import {MatTableModule} from '@angular/material/table';
-import { AppRoutingModule } from './app-routing.module';
-import { ClusterViewComponent } from './cluster-view/cluster-view.component';
+import {AppRoutingModule} from './app-routing.module';
+import {ClusterViewComponent} from './cluster-view/cluster-view.component';
+import {MatList, MatListModule, MatSelectionList} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { ClusterViewComponent } from './cluster-view/cluster-view.component';
   imports: [
     BrowserModule,
     MatTableModule,
+    MatListModule,
     AppRoutingModule,
   ],
   providers: [],
--- src/app/cluster-view/cluster-view.component.ts
+++ src/app/cluster-view/cluster-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Tweet} from '../models/tweet';
 
 @Component({
   selector: 'app-cluster-view',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClusterViewComponent implements OnInit {
 
-  constructor() { }
+  clusterId: string;
+  tweets: Tweet[] = [
+    {id: '1', text: 'tweet 1 asdasdasd asd asd asda sd asd asd asd asd asd', approved: false},
+    {id: '2', text: 'tweet 2 asdasdasd asd asd asda sd asd asd asd asd asd', approved: false},
+  ];
 
-  ngOnInit(): void {
-    console.log('aaaaaaa');
+  constructor(
+    private router: ActivatedRoute
+  ) {
   }
 
+  ngOnInit(): void {
+    this.clusterId = this.router.snapshot.paramMap.get('id');
+  }
+
+  approveTweet(tweet: Tweet) {
+    console.log(`approve for tweet ${tweet.id}`);
+  }
 }
